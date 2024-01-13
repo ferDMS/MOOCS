c02f3c5d90a5a42e54d7aa36aa3c3ac193cbe568
FerDMS <fernandodms04@gmail.com>
2024-01-12 22:30:07 -0600

    Worked Relational SQL Example

--- Using Databases with Python/cw3.sql
+++ Using Databases with Python/cw3.sql
@@ -1,9 +1,33 @@
-INSERT INTO Artist(name) VALUES ('Led Zepellin'), ('ACDC')
-INSERT INTO Genre(name) VALUES ('Rock'), ('Metal')
-INSERT INTO Album(artistId, title) VALUES (2, 'Who Made Who'), (1, 'IV')
-INSERT INTO Track(title, rating, len, count, albumId, genreId) VALUES
-	('Black Dog', 5, 297, 0, 2, 1),
-	('Stairway', 5, 482, 0, 2, 1),
-	('About to Rock', 5, 313, 0, 1, 2),
-	('Who Made Who', 5, 207, 0, 1, 2)
-UPDATE Track SET artistId = Album.artistId FROM Album JOIN Track ON Album.albumId = Track.albumId
+-- Drop tables already existing (and their values)
+DROP TABLE IF EXISTS cw3_Track;
+DROP TABLE IF EXISTS cw3_Album;
+DROP TABLE IF EXISTS cw3_Artist;
+DROP TABLE IF EXISTS cw3_Genre;
+
+-- Based on a schema, create the tables and describe relations with keys
+CREATE TABLE cw3_Genre (
+	genreId INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	name VARCHAR(50)
+);
+
+CREATE TABLE cw3_Artist (
+	artistId INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	name VARCHAR(50)
+);
+
+CREATE TABLE cw3_Album (
+	albumId INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	artistId INT FOREIGN KEY REFERENCES cw3_Artist(artistId),
+	title VARCHAR(100)
+);
+
+CREATE TABLE cw3_Track (
+	trackId INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	albumId INT FOREIGN KEY REFERENCES cw3_Album(albumId),
+	artistId INT FOREIGN KEY REFERENCES cw3_Artist(artistId),
+	genreId INT FOREIGN KEY REFERENCES cw3_Genre(genreId),
+	title VARCHAR(100),
+	len INT,
+	rating INT,
+	count INT
+);
--- Using Databases with Python/cw4.sql
+++
@@ -1,3 +0,0 @@
-SELECT Artist.name, Genre.name FROM Track INNER JOIN Genre ON Track.genreId = Genre.genreId INNER JOIN Album ON Track.albumId = Album.albumId INNER JOIN Artist ON Album.artistId = Artist.artistId;
-
-SELECT Artist.name, Genre.name FROM Track, Genre, Album, Artist WHERE Track.genreId = Genre.genreId AND Track.albumId = Album.albumId AND Album.artistId = Artist.artistId;
